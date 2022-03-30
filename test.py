91ffdce48c902db466bf68bd96fa2621aabd34c2
Fo4doG <7249901@gmail.com>
2022-03-30 22:53:29 +0300

    upgrade test(right hw lesson 4)

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 list1 = list(map(int, input().split()))
 list2 = list(map(int, input().split()))
-for i in list1:
-    for j in list2:
-        if i == j:
-            list1.remove(i)
+for i in list1[:]:
+    if i in list2:
+        list1.remove(i)
 print(list1)
 print(list2)
