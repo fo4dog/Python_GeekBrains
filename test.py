d34a15df387c12fa8a3c32b2e36fcc9e5d7918b7
Fo4doG <7249901@gmail.com>
2022-03-30 22:27:26 +0300

    upgrade test(hw lesson 4)

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-print('hello world')
-print('test')
-a = int(input())
-if a >= 3:
-    print('a>3')
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+for i in list1:
+    for j in list2:
+        if i == j:
+            list1.remove(i)
+print(list1)
+print(list2)
